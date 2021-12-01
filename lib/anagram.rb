dfa4d56f8a7716e6e531b3ab3449c0376e05fde9
Michael Neis <michael.neis12@gmail.com>
2021-11-30 20:58:09 -0600

    commit tree ce2355d98bc945f71b4967f6f0291d38519ef737

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter{|opt| opt.chars.sort == self.word.chars.sort}
+    end
+
+end
